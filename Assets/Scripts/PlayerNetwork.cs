c20c96f60a7945707e5d23b0d78c2d4340a5c1c2
Austin Vosler <austinpvosler@gmail.com>
2024-08-31 14:07:43 -0400

    Demo RPC Movement

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,40 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    [SerializeField] private int speed = 10;
+
+    [ServerRpc]
+    public void MoveServerRpc(Vector3 direction)
+    {
+        transform.position += direction;
+    }
+
+    [ClientRpc]
+    public void MoveClientRpc(Vector3 direction)
+    {
+        if (IsHost) return;
+        transform.position += direction;
+    }
+
+    private void Update()
+    {
+        if (!IsOwner) return;
+
+        Vector3 direction = new Vector3();
+
+        if (Input.GetKey(KeyCode.W)) direction.z = +1f;
+        if (Input.GetKey(KeyCode.S)) direction.z = -1f;
+        if (Input.GetKey(KeyCode.A)) direction.x = -1f;
+        if (Input.GetKey(KeyCode.D)) direction.x = +1f;
+
+        direction = speed * Time.deltaTime * direction;
+        transform.position += direction;
+
+        if (IsHost)
+            MoveClientRpc(direction);
+        else if (IsClient)
+            MoveServerRpc(direction);
+    }
+}
