58efde432ea24e044480e7f4cd16332babd1469e
aden-tt <106639051+aden-tt@users.noreply.github.com>
2025-01-24 01:12:21 -0500

    Interactable Object names now show on hover
    Fixed the bug where the object name would not change
    the Label text during runtime by changing its visibility
    rather than activating and deactivating the UIDocument.
    
    Hover UI is now fully functional.

--- Assets/Scripts/UI/InteractHoverText.cs
+++ Assets/Scripts/UI/InteractHoverText.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEditor.Rendering.FilterWindow;
 
 // This class handles how to display labels when hovering on Interactable Objects
 public class InteractHoverText : MonoBehaviour
 {
     private PlayerMotor motor; // Publisher of hover events
     [SerializeField] GameObject hoverTextGameObject; // Hover text UI
-    private UIDocument hoverTextUIDoc; // Visual tree of hover text UI
+    private UIDocument hoverTextUIDoc; // Visual tree component of hover text UI
     private Label hoverTextLabel; // The text that displays on hover
+    
 
 
     void Awake()
@@ -15,16 +17,15 @@ public class InteractHoverText : MonoBehaviour
         // Initialize variables
         motor = gameObject.GetComponent<PlayerMotor>();
         hoverTextUIDoc = hoverTextGameObject.GetComponent<UIDocument>();
-        hoverTextLabel = hoverTextUIDoc.rootVisualElement.Q("Overlay").Q<Label>("HoverText");
+        hoverTextLabel = hoverTextUIDoc.rootVisualElement.Q<Label>("HoverText");
 
-        hoverTextGameObject.SetActive(false); // ensures hovertext does not show when first spawning in
+        hoverTextLabel.style.visibility = Visibility.Hidden; // ensures hovertext does not show when first spawning in
 
-        //Debug.Log("interacthovertext.cs is awake");
+        // Subcribe to hover events
         if (motor != null)
         {
             motor.OnInteractableObjectAway += HideHoverText;
             motor.OnInteractableObjectHover += ShowHoverText;
-            Debug.Log("events subscribed!");
 
         }
         else
@@ -32,29 +33,25 @@ public class InteractHoverText : MonoBehaviour
             Debug.Log("motor is null");
         }
     }
-
-    void OnDisable()
-    {
-        motor.OnInteractableObjectHover -= ShowHoverText;
-        motor.OnInteractableObjectAway -= HideHoverText;
-    }
  
-    // name holds the name of a interactable GameObject that the player is looking at
+    // name holds the name of the interactable GameObject that the player is looking at
     public void ShowHoverText(string objectName) 
     {
-         // changes text depending on what player is looking at
-        hoverTextGameObject.SetActive(true);
-        hoverTextLabel.text = objectName;
-        Debug.Log(hoverTextLabel.text);
-
-        // display hovertext
-
+        hoverTextLabel.text = objectName; 
+        hoverTextLabel.style.visibility = Visibility.Visible;
 
     }
 
     public void HideHoverText()
     {
-        hoverTextGameObject.SetActive(false);
+        hoverTextLabel.style.visibility = Visibility.Hidden;
+
+    }
+
+    void OnDisable()
+    {
+        motor.OnInteractableObjectHover -= ShowHoverText;
+        motor.OnInteractableObjectAway -= HideHoverText;
     }
 }
 
